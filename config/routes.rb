4517cba87897e0ae8b0e7840915fe7e38c43bfc7
NunoH4 <berkut0406@gmail.com>
2023-07-08 08:30:09 +0000

    [Add]devise:会員コントローラ・会員用ビュー追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  get 'homes/top'
-  devise_for :members
   root to: 'homes#top'
+  
+  # URL /customers/sign_in ...
+  devise_for :members,skip: [:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+  }
 end
