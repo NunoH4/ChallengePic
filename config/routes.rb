fb48f9ddab4a7578e02bf99261f73e7d9bc7fdf5
NunoH4 <berkut0406@gmail.com>
2023-07-09 06:48:14 +0000

    [Fix]Homesコントローラをpublicディレクトリ内に移動。homesビューディレクトリをpublicディレクトリ内に移動

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,19 @@
 Rails.application.routes.draw do
-  root to: 'homes#top'
-  get 'homes/guideline' => 'homes#guideline'
+
+  get 'posts/new'
+  get 'posts/index'
+  get 'posts/show'
+  get 'posts/edit'
   
   # URL /customers/sign_in ...
   devise_for :members,skip: [:passwords], controllers: {
     registrations: "public/registrations",
     sessions: 'public/sessions'
   }
+  scope module: :public do
+    root 'homes#top'
+    get 'homes/guideline' => 'homes#guideline'
+
+    
+  end
 end
