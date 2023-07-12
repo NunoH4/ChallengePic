e59658a79ff409b986acc9a8df1f92e79f818f87
NunoH4 <berkut0406@gmail.com>
2023-07-12 14:31:16 +0900

    [Change]タイムゾーンをAsia/Tokyoに変更

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,6 @@ module ChallengePic
     Rails.application.config.i18n.default_locale = :ja
     Faker::Config.locale = :ja
     config.time_zone = 'Asia/Tokyo'
-    # config.active_record.default_timezone = :local
+    config.active_record.default_timezone = :local
   end
 end
