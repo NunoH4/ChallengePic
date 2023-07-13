d69eb783ada7a1a06f9a56aac84291af14d14cea
NunoH4 <berkut0406@gmail.com>
2023-07-13 15:10:56 +0900

    [Add]ユーザー詳細ページ、編集ページ実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  
+
   namespace :public do
     get 'members/show'
     get 'members/edit'
   end
-  # URL /customers/sign_in ...
+
   devise_for :members,skip: [:passwords], controllers: {
     registrations: "public/registrations",
     sessions: 'public/sessions'
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
     root 'homes#top'
     get 'homes/guideline' => 'homes#guideline'
 
+    resources :members, only: [:show, :edit, :update]
     resources :posts, only: [:new, :index, :show, :edit, :create, :destroy, :update]
     # get "search_tag" => "posts#search_tag"
   end
