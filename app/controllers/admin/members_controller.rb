08f424b1fca42a4b91d77f82e61161f68ebae07d
NunoH4 <berkut0406@gmail.com>
2023-07-21 18:32:48 +0900

    [Add]管理者会員管理機能

---
+++ app/controllers/admin/members_controller.rb
@@ -0,0 +1,28 @@
+class Admin::MembersController < ApplicationController
+  def index
+    @members = Member.all
+  end
+
+  def show
+    @member = Member.find(params[:id])
+  end
+
+  def edit
+    @member = Member.find(params[:id])
+  end
+
+  def update
+    @member = Member.find(params[:id])
+    if @member.update(member_params)
+      redirect_to admin_member_path(@member.id)
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def member_params
+    params.require(:member).permit(:name, :email, :introduction, :profile_image, :is_deleted)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,19 @@ Rails.application.routes.draw do
   devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
     sessions: "admin/sessions"
   }
+  
+  namespace :admin do
+    resources :members, only: [:index, :show, :edit, :update]
+    
+    # get 'top' => 'homes#top', as: 'top'
+    # get 'search' => 'homes#search', as: 'search'
+    # get 'customers/:customer_id/orders' => 'orders#index', as: 'customer_orders'
+    # resources :customers, only: [:index, :show, :edit, :update]
+    # resources :items, except: [:destroy]
+    # resources :genres, only: [:index, :create, :edit, :update]
+    # resources :orders, only: [:index, :show, :update] do
+    #   resources :order_details, only: [:update]
+  end
 
   devise_for :members,skip: [:passwords], controllers: {
     registrations: "public/registrations",
