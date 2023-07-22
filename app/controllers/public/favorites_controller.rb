ed88954a8691fbb8c98c1029c068c58b34237515
NunoH4 <berkut0406@gmail.com>
2023-07-22 23:40:10 +0900

    [Add]お気に入り機能のアラート表示追加

--- app/controllers/public/favorites_controller.rb
+++ app/controllers/public/favorites_controller.rb
@@ -1,16 +1,15 @@
 class Public::FavoritesController < ApplicationController
   before_action :authenticate_member!
   before_action :set_post
-  
 
-  # お気に入り登録
   def create
     if @post.member_id != current_member.id   # 投稿者本人以外に限定
       @favorite = Favorite.create(member_id: current_member.id, post_id: @post.id)
+    else
+      @error_message = "投稿者自身はお気に入り登録できません。"
     end
   end
   
-  # お気に入り削除
   def destroy
     @favorite = Favorite.find_by(member_id: current_member.id, post_id: @post.id)
     @favorite.destroy
@@ -21,5 +20,4 @@ class Public::FavoritesController < ApplicationController
   def set_post
     @post = Post.find(params[:post_id])
   end
-  
 end
