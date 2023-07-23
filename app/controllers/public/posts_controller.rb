5a23c96fe7d730d834254f2913df8b4b612530a5
NunoH4 <berkut0406@gmail.com>
2023-07-24 00:48:05 +0900

    [Update]ページネーションレイアウト調整

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -7,7 +7,7 @@ class Public::PostsController < ApplicationController
   end
 
   def index
-    @posts = Post.all
+    @posts = Post.all.page(params[:page]).per(6)
     @tag_list=Tag.all
   end
 
--- app/controllers/public/searches_controller.rb
+++ app/controllers/public/searches_controller.rb
@@ -6,7 +6,7 @@ class Public::SearchesController < ApplicationController
     if @range == "ユーザー"
       @members = Member.looks(params[:search], params[:word])
     elsif @range == "Challenge Theme"
-      @posts = Post.looks(params[:search], params[:word])
+      @posts = Post.looks(params[:search], params[:word]).page(params[:page]).per(6)
     else
       @tags = Tag.looks(params[:search], params[:word])
       @posts = @tags.flat_map(&:posts)
