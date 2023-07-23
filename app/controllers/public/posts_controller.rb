5e882e44d3f5154f26bc80e45d1b380400431202
NunoH4 <berkut0406@gmail.com>
2023-07-23 15:22:27 +0900

    [Delete]postsコントローラ不要な記述削除

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -61,8 +61,6 @@ class Public::PostsController < ApplicationController
   end
   
   def search_tag
-    # 検索結果画面でもタグ一覧表示
-    @tag_list = Tag.all
     # 　検索されたタグを受け取る
     @tag = Tag.find(params[:tag_id])
     # 　検索されたタグに紐づく投稿を表示
