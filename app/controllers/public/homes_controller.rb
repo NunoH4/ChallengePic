f07f89e2e6511725784af057ca1843e035e87cc5
NunoH4 <berkut0406@gmail.com>
2023-07-24 13:40:06 +0900

    [Fix]新着表示のコントローラの記述修正

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -2,8 +2,13 @@ class Public::HomesController < ApplicationController
 
   def top
     @daily_theme = Challenge.last&.theme || "no theme"
-    # 毎日4:30にお題が更新されるため、+ 4.hours + 30.minutes
-    @posts = Post.where("created_at >= ?", Time.current.beginning_of_day + 4.hours + 30.minutes)
+    # 毎日4:30にお題が更新されるため
+    if Time.current.hour < 4 || (Time.current.hour == 4 && Time.current.min < 30)
+      from = Time.current.yesterday.beginning_of_day + 4.hours + 30.minutes
+    else
+      from = Time.current.beginning_of_day + 4.hours + 30.minutes
+    end
+    @posts = Post.where("created_at >= ?", from)
   end
 
   def guideline
