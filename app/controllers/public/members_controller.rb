670ad45efb8c68bc2b93d9dc5cadb69db854d8e1
NunoH4 <berkut0406@gmail.com>
2023-07-25 23:35:43 +0900

    [Add]会員詳細タブメニュー内ページネーション実装

--- app/controllers/public/members_controller.rb
+++ app/controllers/public/members_controller.rb
@@ -1,17 +1,18 @@
 class Public::MembersController < ApplicationController
   before_action :correct_member, only: [:update, :edit, :leave, :withdrawal]
-  
+
   def show
     @member = Member.find(params[:id])
-    @posts = @member.posts.order(created_at: :desc).page(params[:page]).per(6) #デフォルトを新着順に
+    @posts = @member.posts.order(created_at: :desc).page(params[:page]).per(12) #デフォルトを新着順に
     favorites = Favorite.where(member_id: @member.id).pluck(:post_id)  # ログイン中のユーザーのお気に入りのpost_idカラムを取得
-    @favorite_list = Post.where(id: favorites).order(created_at: :desc).page(params[:page]).per(6)  # postsテーブルからお気に入り登録済みのレコードを取得
+    @favorite_list = Post.where(id: favorites).order(created_at: :desc).page(params[:page]).per(12)  # postsテーブルからお気に入り登録済みのレコードを取得
+    @tab = params[:tab] || 'post'
   end
 
   def edit
     @member = Member.find(params[:id])
   end
-  
+
   def update
     if @member.update(member_params)
       redirect_to member_path(@member), notice: "You have updated user successfully."
@@ -19,7 +20,7 @@ class Public::MembersController < ApplicationController
       render :edit
     end
   end
-  
+
   def leave
     @member = Member.find(current_member.id)
   end
@@ -31,18 +32,18 @@ class Public::MembersController < ApplicationController
     flash[:notice] = "退会処理を実行いたしました"
     redirect_to root_path
   end
-  
+
   private
 
   def member_params
     params.require(:member).permit(:name, :introduction, :profile_image)
   end
-  
+
   def correct_member
     @member = Member.find(params[:id])
     unless @member == current_member
       redirect_to member_path(current_member)
     end
   end
-  
+
 end
