bec550f437aab5848f8c9bb8cce27a342235b979
NunoH4 <berkut0406@gmail.com>
2023-07-27 01:48:15 +0900

    [Add]未ログインユーザーと管理者がお気に入りしようとした際のアラート表示追加

--- app/controllers/public/favorites_controller.rb
+++ app/controllers/public/favorites_controller.rb
@@ -1,22 +1,25 @@
 class Public::FavoritesController < ApplicationController
-  before_action :authenticate_member!
   before_action :set_post
 
   def create
-    if @post.member_id != current_member.id   # 投稿者本人以外に限定
-      @favorite = Favorite.create(member_id: current_member.id, post_id: @post.id)
+    if member_signed_in?
+      if @post.member_id != current_member.id # 投稿者本人以外に限定
+        @favorite = Favorite.create(member_id: current_member.id, post_id: @post.id)
+      else
+        @error_message = "投稿者自身はお気に入り登録できません"
+      end
     else
-      @error_message = "投稿者自身はお気に入り登録できません"
+      @error_message = "ユーザーログインしてください"
     end
   end
-  
+
   def destroy
     @favorite = Favorite.find_by(member_id: current_member.id, post_id: @post.id)
     @favorite.destroy
   end
-  
+
   private
-  
+
   def set_post
     @post = Post.find(params[:post_id])
   end
